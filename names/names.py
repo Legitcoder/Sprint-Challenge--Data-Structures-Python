23f79a6d9ac142934c3394e2d4579dde68580b84
Moin Uddin <U_Moin75@yahoo.com>
2019-03-01 12:06:35 -0500

    Complete names

--- names/names.py
+++ names/names.py
@@ -1,20 +1,51 @@
 import time
 
+# start_time = time.time()
+#
+# f = open('names_1.txt', 'r')
+# names_1 = f.read().split("\n")  # List containing 10000 names
+# f.close()
+#
+# f = open('names_2.txt', 'r')
+# names_2 = f.read().split("\n")  # List containing 10000 names
+# f.close()
+#
+# duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+#
+# end_time = time.time()
+# print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+# print (f"runtime: {end_time - start_time} seconds")
+
+
+
+
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
-names_1 = f.read().split("\n")  # List containing 10000 names
+names = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 f = open('names_2.txt', 'r')
-names_2 = f.read().split("\n")  # List containing 10000 names
+names.extend(f.read().split("\n"))  # List containing 10000 names
 f.close()
 
+
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+names_dictionary = {}
+
+for name in names:
+    if name not in names_dictionary:
+        names_dictionary[name] = 1
+    else:
+        if names_dictionary[name] == 1: duplicates.append(name)
+        names_dictionary[name] += 1
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
